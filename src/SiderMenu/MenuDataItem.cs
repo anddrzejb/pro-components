322b49c66b00704aeab9c17bc0110d1e31b33e6f
anddrzejb <andrzej@neelyc.com.cy>
2021-02-11 15:57:50 +0100

    feat(module:menudataitem): made name virtual

--- src/SiderMenu/MenuDataItem.cs
+++ src/SiderMenu/MenuDataItem.cs
@@ -5,13 +5,13 @@ namespace AntDesign.Pro.Layout
 {
     public class MenuDataItem
     {
-        public string[] Authority { get; set; }
+        public string[] Authority { get; set; }        
         public MenuDataItem[] Children { get; set; }
         public bool HideChildrenInMenu { get; set; }
         public bool HideInMenu { get; set; }
         public string Icon { get; set; }
         public string Locale { get; set; }
-        public string Name { get; set; }
+        public virtual string Name { get; set; }
         public string Key { get; set; }
         public string Path { get; set; }
         public string[] ParentKeys { get; set; }
